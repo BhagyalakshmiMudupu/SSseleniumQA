7666e51303af5f3b34ed46fb0957c5af43d183c2
Bhagya <bhagyalakshmi.abap@gmail.com>
2023-04-03 21:11:16 -0500

    strings program

---
+++ src/test/java/Day2/Strings.java
@@ -0,0 +1,14 @@
+package Day2;
+
+public class Strings {
+  int a=12;
+  static int b = 10;
+	public static void main(String[] args) {
+		Strings s = new Strings();
+		System.out.println(s.a);
+		System.out.println(b);
+		
+      
+	}
+
+}
